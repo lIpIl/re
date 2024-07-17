7078a88eb3432e0a5c37aafd474ef7a2d043d60b
lIpIl <79177548+lIpIl@users.noreply.github.com>
2024-07-17 23:28:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import torch
 from PIL import Image
@@ -25,4 +26,5 @@ def detect():
     return jsonify({'image': encoded_img})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
